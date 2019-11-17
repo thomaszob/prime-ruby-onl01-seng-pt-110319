f3cb180c111590844531c7fecd91b4d69b137119
Thomas Zobrosky <thomaszobrosky@gmail.com>
2019-11-17 06:37:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,28 @@
 # Add  code here!
+def prime?(number)
+  #array = [2..(number - 1)]
+  #truth_array = []
+  if number != number.abs or number == 0 or number == 1 
+    return false 
+  end 
+  (2..(number - 1)).each{|i|
+    if number % i == 0 
+      return false
+    end 
+  }
+  
+  return true 
+  #(2..(number -1)).each{|i| 
+  #  if number % i == 0 
+  #    truth_array << false
+  #  else 
+  #    truth_array << true
+  #  end
+  #}
+  
+  #if truth_array.include?(false)
+  #  return true
+  #else
+  #  return false
+  #end
+end 
